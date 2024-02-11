a1be02c54f827790df9b0bccf52391360a3f4d8a
Naveen <snaveen935@gmail.com>
2024-02-11 20:50:22 +0530

    Fix archives base name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         versionName = project.libs.versions.app.version.versionName.get()
         versionCode = project.libs.versions.app.version.versionCode.get().toInt()
         vectorDrawables.useSupportLibrary = true
-        setProperty("archivesBaseName", "gallery-$versionCode")
+        setProperty("archivesBaseName", "music-player-$versionCode")
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
         }
